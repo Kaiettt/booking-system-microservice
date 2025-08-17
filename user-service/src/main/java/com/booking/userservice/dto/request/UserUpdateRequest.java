b3e13c3a04920f652a96b3dd3947812b4aa1e7a7
Kaiettt <nguyenanhkiet845@gmail.com>
2025-08-17 13:21:18 +0000

    feat: api-gateway

--- user-service/src/main/java/com/booking/userservice/dto/request/UserUpdateRequest.java
+++ user-service/src/main/java/com/booking/userservice/dto/request/UserUpdateRequest.java
@@ -1,6 +1,5 @@
 package com.booking.userservice.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
